f2a285d1f218f9749885aeb279c855582f06098a
Rob Hutchin <rob@hutchin.co.uk>
2023-12-20 22:11:28 +0000

    Add more unit tests for create customer

---
+++ SalesWeb.UnitTests/Services/CustomerServiceTests.cs
@@ -0,0 +1,97 @@
+using System.ComponentModel.DataAnnotations;
+using Moq;
+using SalesWeb.Domain.Entities;
+using SalesWeb.Domain.Interfaces;
+using SalesWeb.Domain.Services;
+
+namespace SalesWeb.UnitTests;
+
+public class CustomerServiceTests
+{
+    private readonly Mock<ICustomerRepository> _mockRepository;
+    private readonly CustomerService _customerService;
+
+    public CustomerServiceTests()
+    {
+        _mockRepository = new Mock<ICustomerRepository>();
+        _customerService = new CustomerService(_mockRepository.Object);
+    }
+    
+    [Fact]
+    public async Task CreateCustomer_WithValidDetails_ReturnsCreatedCustomer()
+    {
+        var customer = await _customerService.CreateCustomer("Rob", "Smith", "07954111222", "rob.smit@email.com");
+        Assert.NotNull(customer);
+    }
+    
+    [Fact]
+    public async Task CreateCustomer_WithValidDetails_SetsAllPropertiesCorrectly()
+    {
+        var customer = await _customerService.CreateCustomer("Rob", "Smith", "07954111222", "rob.smit@email.com");
+        Assert.Equal("Rob", customer.FirstName);
+        Assert.Equal("Smith", customer.LastName);
+        Assert.Equal("rob.smit@email.com", customer.Email);
+        Assert.Equal("07954111222", customer.Phone);
+    }
+    
+    [Fact]
+    public async Task CreateCustomer_WithValidDetails_CallsCreateOnlyOnce()
+    {
+        var customer = await _customerService.CreateCustomer("Rob", "Smith", "07954111222", "rob.smit@email.com");
+        _mockRepository.Verify(repo => repo.CreateAsync(It.IsAny<Customer>()), Times.Once);
+    }
+    
+    [Fact]
+    public async Task CreateCustomerAsync_WithInvalidEmail_ThrowsValidationException()
+    {
+        var exception = await Assert.ThrowsAsync<ValidationException>(() => 
+            _customerService.CreateCustomer("Rob", "Smith","07954111222", "rob.smit@email.com"));
+        
+        Assert.Equal("Invalid email format", exception.Message);
+    }
+    
+    [Fact]
+    public async Task CreateCustomerAsync_WithNullFirstName_ThrowsArgumentNullException()
+    {
+        var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => 
+            _customerService.CreateCustomer(null, "Doe", "07954111222", "rob.smit@email.com"));
+
+        Assert.Equal("firstName", exception.ParamName);
+    }
+    
+    [Fact]
+    public async Task CreateCustomerAsync_WithNullLastName_ThrowsArgumentNullException()
+    {
+        var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => 
+            _customerService.CreateCustomer("Rob", null, "07954111222", "rob.smit@email.com"));
+
+        Assert.Equal("firstName", exception.ParamName);
+    }
+    
+    [Fact]
+    public async Task CreateCustomerAsync_WithNullPhone_ThrowsArgumentNullException()
+    {
+        var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => 
+            _customerService.CreateCustomer("Rob", "Smith","07954111222", "rob.smit@email.com"));
+
+        Assert.Equal("firstName", exception.ParamName);
+    }
+    
+    [Fact]
+    public async Task CreateCustomerAsync_WithNullMobile_ThrowsArgumentNullException()
+    {
+        var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => 
+            _customerService.CreateCustomer("Rob", "Smith","07954111222", "rob.smit@email.com"));
+
+        Assert.Equal("firstName", exception.ParamName);
+    }
+    
+    [Fact]
+    public async Task CreateCustomerAsync_WithNullEmail_ThrowsArgumentNullException()
+    {
+        var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => 
+            _customerService.CreateCustomer("Rob", "Smith","07954111222", null));
+
+        Assert.Equal("firstName", exception.ParamName);
+    }
+}
